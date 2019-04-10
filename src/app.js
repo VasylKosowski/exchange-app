3cb54ed6bd643a2ef3177c418650e4df7f5b2c6e
sg0226637 <vasyl.kosovskyi@sabre.com>
2019-04-10 17:30:29 +0200

    - a bit organize;

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './stylesheets/app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <p>
+            Hello
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
