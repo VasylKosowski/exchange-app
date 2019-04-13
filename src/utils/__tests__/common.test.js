1822613e601098566e66415a415cee44c70f848c
sg0226637 <vasyl.kosovskyi@sabre.com>
2019-04-14 00:32:13 +0200

    - updated tests;

--- src/utils/__tests__/common.test.js
+++ src/utils/__tests__/common.test.js
@@ -1,8 +1,14 @@
-import { isSuccess } from '../common';
+import { isSuccess, isEmptyOrZero } from '../common';
 
-describe('Component: AppComponent', () => {
+describe('utils', () => {
     it('should handle isSuccess properly', () => {
         expect(isSuccess({ success: true })).to.eql(true);
         expect(isSuccess({ success: false })).to.eql(false);
+    });
+
+    it('should handle isEmptyOrZero properly', () => {
+        expect(isEmptyOrZero('')).to.eql(true);
+        expect(isEmptyOrZero(0)).to.eql(true);
+        expect(isEmptyOrZero('0')).to.eql(false);
     });
 });
